6e157b35b23bd2a62536095dd69594d15f9293f6
if2012 <735688270@qq.com>
2018-06-04 17:16:42 +0800

    mystrcmp

---
+++ strcmp.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int MyStrcmp(char *source, char *dest)
+{
+    if (source == NULL || dest == NULL)
+        return -1;
+
+    while (*source && *dest)
+    {
+        if (*source++ != *dest++)
+            return -1;
+    }
+    
+    if (!*source && !*dest)
+        return 0;
+    else
+        return -1;
+}
+
+int main()
+{
+    char source[50]  = {0xff};
+    char dest[50] = {0xff};
+
+    scanf("%s", source);
+    scanf("%s", dest);
+
+    printf("result:%d\n", MyStrcmp(source, dest));
+
+    return 0;
+}
